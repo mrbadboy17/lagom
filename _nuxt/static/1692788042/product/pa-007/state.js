fb9fc227026d0cc807428bb493c910bd4cc28e19
mrbadboy17 <24917693+mrbadboy17@users.noreply.github.com>
2023-08-23 14:35:15 +0330

    1528e34

---
+++ _nuxt/static/1692788042/product/pa-007/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692788042",layout:"default",error:a,state:{likes:{likes:[]}},serverRendered:true,routePath:"\u002Fproduct\u002Fpa-007",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b522291d"}}}}(null));
---
+++ _nuxt/static/1692788042/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692788042",layout:"default",error:a,state:{likes:{likes:[]}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b522291d"}}}}(null,"\u002F"));
